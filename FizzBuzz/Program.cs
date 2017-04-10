e933cf2f890cbd3aeda7bf2303b4010ba67b6f13
Gustavo Veloso <gveloso@comscore.com>
2017-04-10 18:59:20 -0300

    Initial commit with partial solution

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 0; i < 100; i++)
+            {
+                Console.WriteLine(EvaluaFizzBuzz(i));
+            }
+
+            Console.ReadLine();
+        }
+
+        public static string EvaluaFizzBuzz(int numero)
+        {
+            double result;
+            result = numero / 3.0;
+            int resultInt;
+            if (int.TryParse(result.ToString(), out resultInt))
+            {
+                return "Fizz";
+            }
+
+            return numero.ToString();
+          
+        }
+    }
+}
---
+++ FizzBuzzTests/FizzBuzzTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FizzBuzz;
+
+namespace FizzBuzzTests
+{
+    [TestClass]
+    public class FizzBuzzTests
+    {
+        [TestMethod]
+        public void PrintFizzWhen3()
+        {
+            var numero = 3;
+            Assert.AreEqual("Fizz", Program.EvaluaFizzBuzz(numero));
+        }
+        [TestMethod]
+        public void PrintNoFizzWhen5()
+        {
+            var numeroNo = 5;
+            Assert.AreNotEqual("Fizz", Program.EvaluaFizzBuzz(numeroNo));
+        }
+    }
+}
